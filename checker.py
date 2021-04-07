2bb43a85b261fc3b7b62f5ccbb345b1a37caffd5
Mateusz Biernacki <mateuszbiernacki@icloud.com>
2021-04-07 09:55:21 +0200

    Checking available space and sending mail

--- checker.py
+++ checker.py
@@ -6,9 +6,11 @@ if sys.platform == 'linux':
     result = os.statvfs("/")
     free_space_percent = float(result.f_bavail / result.f_blocks * 100)
     free_space_gigabytes = (result.f_bavail * result.f_bsize) / 1024 / 1024 / 1024
-    print(100-free_space_percent)
-    print(free_space_gigabytes)
-    smtp.send_email('mateusz.biernacki@banachoutsourcing.pl', 'Available space', 'Available space: %sGB (its %s of entire disk).' % (free_space_gigabytes, free_space_percent))
+    if free_space_percent > 90:
+        smtp.send_email('mateusz.biernacki@banachoutsourcing.pl',
+                        'Available space',
+                        'Available space: %sGB (its %s of entire disk).' %
+                        (round(free_space_gigabytes, 4), round(free_space_percent, 4)))
 
 
 else:
